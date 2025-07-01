31e90dfa83dc5ef735873624fadf1d27f848b6ff
jhaaadarsh7 <jhaaadarsh582@gamail.com>
2025-07-01 11:05:27 +0545

    profile added

--- server/route/authRoutes.js
+++ server/route/authRoutes.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const { signup, login } = require('../controllers/authController');
+const user = require('../models/user');
+const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/signup', signup);
 router.post('/login', login);
+router.get('/me', authMiddleware, async (req, res) => {
+  try {
+    const user = await user.findById(req.user.id).select('-password'); // exclude password
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    res.json(user);
+  } catch (err) {
+    console.error('Profile error:', err);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
 
 module.exports = router;
